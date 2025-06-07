55738b855c156dd3e24310dbcaf1e0d58e5885bb
convexMind <mayenbergerj@gmail.com>
2025-06-07 17:11:24 +0200

    added Point, Line class. Added line drawing functionality

--- graphics.py
+++ graphics.py
@@ -2,24 +2,40 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window():
     def __init__(self, width, height):
-        self.root = Tk()
-        self.root.title("Maze Solver")
-        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
-        self.canvas.pack(fill=BOTH, expand=1)
-        self.running = False
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
     def redraw(self):
-        self.root.update()
-        self.root.update_idletasks()
+        self.__root.update()
+        self.__root.update_idletasks()
     
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
         print("window closed...")
 
     def close(self):
-        self.running = False
+        self.__running = False
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, p1, p2):
+        self.__p1 = p1
+        self.__p2 = p2
+    
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.__p1.x, self.__p1.y, self.__p2.x, self.__p2.y, fill=fill_color, width=2)
 
 
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from graphics import Window
+from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    
+    
+    a, b, c, d = Point(0,0), Point(800, 600), Point(50, 100), Point(500, 300)
+    l1, l2, l3, l4, l5, l6 = Line(a, b), Line(a, c), Line(a, d), Line(b, c), Line(b, d), Line(c, d)
+    win.draw_line(l1, "black")
+    win.draw_line(l2, "red")
+    win.draw_line(l3, "green")
+    win.draw_line(l4, "blue")
+    win.draw_line(l5, "grey")
+    win.draw_line(l6, "yellow")
+
     win.wait_for_close()
 
 
